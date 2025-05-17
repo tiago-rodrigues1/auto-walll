1d4b756541f2371bff96e21c8a6c38a43ab146b8
tiago-rodrigues1 <tiagords127@gmail.com>
2025-05-16 22:11:24 -0300

    feat: add image themes

--- utils/get-image.js
+++ utils/get-image.js
@@ -3,13 +3,29 @@ import axios from "axios"
 
 import { config } from "../config.js"
 
+function getRandomNumberInRange(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const getThemeQuery = () => {
+	if (config.THEMES) {
+		const themeIndex = getRandomNumberInRange(0, (config.THEMES.length - 1))
+		const theme = config.THEMES[themeIndex]
+
+		return `&query=${theme}`
+	}
+
+	return ""
+}
+
 export const getImage = async () => {
 	try {
-		const url = `https://api.unsplash.com/photos/random/?client_id=${config.API_KEY}&orientation=landscape&query=space`
+		const themeQuery = getThemeQuery()
+		const url = `https://api.unsplash.com/photos/random/?client_id=${config.API_KEY}&orientation=landscape${themeQuery}`
 		const response = await axios.get(url)
 
 		if (response.status === 200) {
-			console.log("[OK] Image retrieved successfully")
+			console.log(`[OK] Image with retrieved successfully ${themeQuery}`)
 
 			const image = {
 				filename: `${response.data.id}.jpg`,
