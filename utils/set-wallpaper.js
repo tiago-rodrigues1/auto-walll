0c738f545ad7abef3a21668c1c24336e91e1ef1f
tiago-rodrigues1 <tiagords127@gmail.com>
2025-05-16 21:58:38 -0300

    feat: set the image as wallpaper

---
+++ utils/set-wallpaper.js
@@ -0,0 +1,23 @@
+import shell from "shelljs"
+
+export const setWallpaper = async (imagePath) => {
+	try {
+		const display = process.env.DISPLAY || ":0"
+    const dbus = process.env.DBUS_SESSION_BUS_ADDRESS || `unix:path=/run/user/${process.getuid()}/bus`
+
+		console.log(imagePath)
+    const setCommand = (key) =>
+      `DISPLAY=${display} DBUS_SESSION_BUS_ADDRESS=${dbus} gsettings set org.gnome.desktop.background ${key} "file://${imagePath}"`
+
+    const result1 = shell.exec(setCommand("picture-uri")).code
+    const result2 = shell.exec(setCommand("picture-uri-dark")).code
+
+		if (result1 === 0 && result2 === 0) {
+			console.log("[OK] New wallpaper set successfully")
+		}
+
+
+	} catch (error) {
+		console.error("[ERROR] Set wallpaper failed:", error)
+	}
+}
